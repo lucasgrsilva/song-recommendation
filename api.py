fd1c9a0ee43ddc6e0815590d2dfd14eaa625372b
Lucas Gabriel Rios Silva <lucasgriosilva@gmail.com>
2025-01-12 12:32:49 -0300

    Configura env vars e config maps

--- api.py
+++ api.py
@@ -1,16 +1,33 @@
 import pickle
+import os
+import hashlib
+import time
+import datetime
+import threading
 from flask import Flask, request, jsonify, send_file
 
 app = Flask(__name__)
+MODEL_PATH = os.getenv("MODEL_PATH")
+APP_VERSION = os.getenv("APP_VERSION")
+last_checksum = None
+model_last_update = None
+model = None
 
-with open('/app/recommendation/recommendation_model.pickle', 'rb') as model_file:
-    app.model = pickle.load(model_file)
+def load_model():
+    global model
+    print(f"Loading model from {MODEL_PATH}")
+    with open(MODEL_PATH, "rb") as f:
+        model = pickle.load(f)
+
+def get_file_checksum(filepath):
+    with open(filepath, "rb") as f:
+        return hashlib.md5(f.read()).hexdigest()        
 
 def recommend_songs(input_songs):
-    model_data = app.model
+    global model
     recommended_songs = set()
 
-    track_rules = model_data["rules"]
+    track_rules = model["rules"]
 
     for rule in track_rules:
         if set(rule[0]).issubset(input_songs):
@@ -19,12 +36,13 @@ def recommend_songs(input_songs):
     recommended_songs = recommended_songs - set(input_songs)
 
     if not recommended_songs:
-        recommended_songs = [song for song in model_data["track_popularity"].index if song not in input_songs]
+        recommended_songs = [song for song in model["track_popularity"].index if song not in input_songs]
   
     return list(recommended_songs)
 
 @app.route('/api/recommend', methods=['POST'])
 def recommend():
+    global model_last_update
     data = request.get_json()
     input_songs = data.get('songs', [])
     
@@ -42,11 +60,26 @@ def recommend():
 
     response = {
         "songs": recommended_songs,
-        # "version": app.model["metadata"]["version"],
-        # "model_date": app.model["metadata"]["last_update"]
+        "version": APP_VERSION,
+        "model_date": datetime.datetime.fromtimestamp(model_last_update).strftime('%Y-%m-%d %H:%M:%S')
     }
 
     return jsonify(response)
 
+def model_watcher():
+    global last_checksum
+    global model_last_update
+    while True:
+        if os.path.exists(MODEL_PATH):
+            new_checksum = get_file_checksum(MODEL_PATH)
+            if new_checksum != last_checksum:
+                print("Updating model :)")
+                last_checksum = new_checksum
+                model_last_update = time.time()
+                load_model()
+        time.sleep(5)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    watcher_thread = threading.Thread(target=model_watcher, daemon=True)
+    watcher_thread.start()
+    app.run()
--- song_recommendation_model_generator.py
+++ song_recommendation_model_generator.py
@@ -1,9 +1,9 @@
 import pandas as pd
+import os
+import argparse
 import pickle
 from fpgrowth_py import fpgrowth
 
-DATASET_1 = "/app/datasets/2023_spotify_ds1.csv"
-DATASET_2 = "/app/datasets/2023_spotify_ds2.csv"
 MODEL_FILE = "/app/datasets/recommendation_model.pickle"
 
 def load_data(file_path):
@@ -15,32 +15,34 @@ def generate_rules(transactions, min_sup=0.1, min_conf=0.3):
     freq_item_set, rules = fpgrowth(transactions, minSupRatio=min_sup, minConf=min_conf)
     return freq_item_set, rules
 
-print(f"Loading data from {DATASET_1}...")
-transactions_ds1 = load_data(DATASET_1)
+def main(dataset_path):
+    print(f"Dataset Path: {dataset_path}")
 
-print("Training model with dataset 1...")
-freq_item_set_ds1, rules_ds1 = generate_rules(transactions_ds1)
+    if not os.path.exists(dataset_path):
+        print(f"Error: Dataset path '{dataset_path}' does not exist.")
+        return
 
-model = {
-    "freq_item_set": freq_item_set_ds1,
-    "rules": rules_ds1,
-    "track_popularity": {}, 
-}
-print(f"Initial model trained with {len(rules_ds1)} rules.")
+    print(f"Loading data from {dataset_path}...")
+    transactions_ds = load_data(dataset_path)
 
-if DATASET_2:
-    print(f"Loading data from {DATASET_2}...")
-    transactions_ds2 = load_data(DATASET_2)
-    
-    print("Updating model with dataset 2...")
-    freq_item_set_ds2, rules_ds2 = generate_rules(transactions_ds2)
-    
-    model["rules"].extend(rules_ds2)
-    model["freq_item_set"].extend(freq_item_set_ds2)
-    print(f"Updated model with {len(rules_ds2)} additional rules.")
+    print("Training model with dataset...")
+    freq_item_set_ds, rules_ds = generate_rules(transactions_ds)
 
-print(f"Saving model to {MODEL_FILE}...")
-with open(MODEL_FILE, "wb") as model_file:
-    pickle.dump(model, model_file)
+    model = {
+        "freq_item_set": freq_item_set_ds,
+        "rules": rules_ds,
+        "track_popularity": {}, 
+    }
+    print(f"Model trained with {len(rules_ds)} rules.")
 
-print(f"Model saved! Total rules: {len(model['rules'])}")
+    print(f"Saving model to {MODEL_FILE}...")
+    with open(MODEL_FILE, "wb") as model_file:
+        pickle.dump(model, model_file)
+
+    print(f"Model saved! Total rules: {len(model['rules'])}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset-path', type=str, required=True, help="Path to the dataset")
+    args = parser.parse_args()
+    main(args.dataset_path)
